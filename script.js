752efb0ac2a17c499c055531ed1338342add9292
oleg <krevets.olegg@gmail.com>
2023-02-26 12:01:53 +0200

    main

---
+++ script.js
@@ -0,0 +1,21 @@
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+}
+
+
+let result = '';
+const lenght = 7;
+
+for (let i = 1; i < length; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += "*";
+    }
+
+    result += '\n';
+}
+
+console.log(result);
