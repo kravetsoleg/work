ed23c99d439e58f7a668dc19cc2bdb964e8ee5db
oleg <krevets.olegg@gmail.com>
2023-04-09 23:48:22 +0300

    main

---
+++ project1/metods.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// if (4 == 4) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error');
+// }
+
+
+const str = 'test';
+
+console.log (str.toUpperCase());
+console.log (str);
+
+const fruit = "Some fruit";
+
+console.log (fruit.indexOf('fruit'));
+
+const logg = "Hello world";
+
+// console.log (logg.slice(6, 11));
+
+// console.log (logg.substring(6, 11));
+
+console.log (logg.substr(6, 5));
+
+const num = 12.2;
+console.log (Math.round(num));
